12a59f47e6263bf515ba76c29a6b0ee55b835265
Wackyzane <karlsen.per.a@gmail.com>
2025-01-22 16:01:20 -0700

    Added Table and Visual UI

--- ExpenseTracker/Form1.Designer.cs
+++ ExpenseTracker/Form1.Designer.cs
@@ -28,12 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            ExitButton = new Label();
+            File = new Label();
+            WindowDragBox = new Panel();
+            DataGrid = new DataGridView();
+            AddColumnButton = new Button();
+            AddColumnName = new TextBox();
+            WindowDragBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)DataGrid).BeginInit();
+            SuspendLayout();
+            // 
+            // ExitButton
+            // 
+            ExitButton.AutoSize = true;
+            ExitButton.Font = new Font("Segoe UI", 12F);
+            ExitButton.ForeColor = Color.FromArgb(161, 161, 161);
+            ExitButton.Location = new Point(767, 7);
+            ExitButton.Name = "ExitButton";
+            ExitButton.Size = new Size(19, 21);
+            ExitButton.TabIndex = 0;
+            ExitButton.Text = "X";
+            ExitButton.TextAlign = ContentAlignment.MiddleCenter;
+            ExitButton.Click += ExitButtonClick;
+            ExitButton.MouseEnter += ExitButtonMouseEnter;
+            ExitButton.MouseLeave += ExitButtonMouseLeave;
+            // 
+            // File
+            // 
+            File.AutoSize = true;
+            File.Font = new Font("Segoe UI", 12F);
+            File.ForeColor = Color.FromArgb(161, 161, 161);
+            File.Location = new Point(10, 7);
+            File.Name = "File";
+            File.Size = new Size(34, 21);
+            File.TabIndex = 1;
+            File.Text = "File";
+            File.MouseEnter += FileMouseEnter;
+            File.MouseLeave += FileMouseLeave;
+            // 
+            // WindowDragBox
+            // 
+            WindowDragBox.BackColor = Color.Transparent;
+            WindowDragBox.Controls.Add(File);
+            WindowDragBox.Controls.Add(ExitButton);
+            WindowDragBox.Location = new Point(2, 2);
+            WindowDragBox.Name = "WindowDragBox";
+            WindowDragBox.Size = new Size(798, 34);
+            WindowDragBox.TabIndex = 2;
+            WindowDragBox.MouseDown += DragWindow;
+            // 
+            // DataGrid
+            // 
+            DataGrid.BackgroundColor = Color.FromArgb(31, 31, 31);
+            DataGrid.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            DataGrid.GridColor = Color.FromArgb(161, 161, 161);
+            DataGrid.Location = new Point(12, 71);
+            DataGrid.Name = "DataGrid";
+            DataGrid.Size = new Size(776, 367);
+            DataGrid.TabIndex = 2;
+            DataGrid.CellEndEdit += EditTable;
+            // 
+            // AddColumnButton
+            // 
+            AddColumnButton.Location = new Point(12, 42);
+            AddColumnButton.Name = "AddColumnButton";
+            AddColumnButton.Size = new Size(84, 23);
+            AddColumnButton.TabIndex = 3;
+            AddColumnButton.Text = "Add Column";
+            AddColumnButton.UseVisualStyleBackColor = true;
+            AddColumnButton.Click += AddColumnClick;
+            // 
+            // AddColumnName
+            // 
+            AddColumnName.Location = new Point(102, 42);
+            AddColumnName.Name = "AddColumnName";
+            AddColumnName.Size = new Size(100, 23);
+            AddColumnName.TabIndex = 4;
+            AddColumnName.KeyDown += AddColumnEnter;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(31, 31, 31);
+            ClientSize = new Size(800, 450);
+            Controls.Add(AddColumnName);
+            Controls.Add(AddColumnButton);
+            Controls.Add(DataGrid);
+            Controls.Add(WindowDragBox);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            Text = "Form1";
+            WindowDragBox.ResumeLayout(false);
+            WindowDragBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)DataGrid).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label ExitButton;
+        private Label File;
+        private Panel WindowDragBox;
+        private DataGridView DataGrid;
+        private Button AddColumnButton;
+        private TextBox AddColumnName;
     }
 }
--- ExpenseTracker/Form1.cs
+++ ExpenseTracker/Form1.cs
@@ -1,10 +1,134 @@
 namespace ExpenseTracker
 {
+    using System.Data;
+    using System.Runtime.InteropServices;
     public partial class Form1 : Form
     {
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        /// <summary>
+        /// DllImportAttribute allows you to call on unmanaged code from outside the program.
+        /// user32.dll has most for the core Windows API in System32 Folder
+        /// Windows forms uses user32.dll to create its form
+        /// We are bypassing working with Windows Forms here to directly use the function it uses to move the window as a whole
+        /// </summary>
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
         public Form1()
         {
             InitializeComponent();
+            CreateTable();
+        }
+
+        private void CreateTable()
+        {
+            DataTable dt = new DataTable();
+            // Add Columns
+            dt.Columns.Add("Months", typeof(string));
+            dt.Columns.Add("Rent", typeof(double));
+            dt.Columns.Add("Bills", typeof(double));
+            dt.Columns.Add("Subscriptions", typeof(double));
+            dt.Columns.Add("Groceries", typeof(double));
+            dt.Columns.Add("Other", typeof(double));
+            dt.Columns.Add("Total", typeof(double));
+
+            // Add Rows
+            dt.Rows.Add("January", 0, 0, 0, 0, 0, 0);
+            dt.Rows.Add("February", 0, 0, 0, 0, 0, 0);
+            dt.Rows.Add("March", 0, 0, 0, 0, 0, 0);
+            dt.Rows.Add("April", 0, 0, 0, 0, 0, 0);
+            dt.Rows.Add("May", 0, 0, 0, 0, 0, 0);
+            dt.Rows.Add("June", 0, 0, 0, 0, 0, 0);
+            dt.Rows.Add("July", 0, 0, 0, 0, 0, 0);
+            dt.Rows.Add("August", 0, 0, 0, 0, 0, 0);
+            dt.Rows.Add("September", 0, 0, 0, 0, 0, 0);
+            dt.Rows.Add("October", 0, 0, 0, 0, 0, 0);
+            dt.Rows.Add("November", 0, 0, 0, 0, 0, 0);
+            dt.Rows.Add("December", 0, 0, 0, 0, 0, 0);
+            dt.Rows.Add("Total", 0, 0, 0, 0, 0, 0);
+            DataGrid.DataSource = dt;
+            DataGrid.AllowUserToAddRows = false;
+            DataGrid.AllowUserToDeleteRows = false;
+            DataGrid.AllowUserToOrderColumns = true;
+        }
+
+        private void ExitButtonClick(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void ExitButtonMouseEnter(object sender, EventArgs e)
+        {
+            ExitButton.BackColor = Color.FromArgb(99, 95, 91);
+        }
+
+        private void DragWindow(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void ExitButtonMouseLeave(object sender, EventArgs e)
+        {
+            ExitButton.BackColor = Color.Transparent;
+        }
+
+        private void FileMouseEnter(object sender, EventArgs e)
+        {
+            File.BackColor = Color.FromArgb(99, 95, 91);
+        }
+
+        private void FileMouseLeave(object sender, EventArgs e)
+        {
+            File.BackColor = Color.Transparent;
+        }
+
+        private void AddColumnClick(object sender, EventArgs e)
+        {
+            AddColumnName.Text = string.Empty;
+            CreateTable();
+        }
+
+        private void AddColumnEnter(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                AddColumnClick(sender, e);
+            }
+        }
+
+        private void EditTable(object sender, DataGridViewCellEventArgs e)
+        {
+            RowTotal(DataGrid.Rows[e.RowIndex]);
+            ColumnTotal(DataGrid.Columns[e.ColumnIndex]);
+        }
+
+        private void RowTotal(DataGridViewRow row)
+        {
+            double total = 0d;
+
+            for (int i = 0; i < DataGrid.Columns.Count; i++)
+            {
+                if (i == DataGrid.Columns.Count - 1) continue;
+                if (double.TryParse(row.Cells[i].Value?.ToString(), out double cellValue))
+                {
+                    total += cellValue;
+                }
+            }
+
+            row.Cells["Total"].Value = total.ToString();
+        }
+
+        private void ColumnTotal(DataGridViewColumn column)
+        {
+            
         }
     }
 }
